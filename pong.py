1fba0a46b575d32b2b243c0cf81987075c16444a
123pythonprojects <138014682+123pythonprojects@users.noreply.github.com>
2023-06-28 05:26:40 -0700

    Update pong.py

--- pong.py
+++ pong.py
@@ -2,148 +2,90 @@ import pygame
 from pygame.locals import *
 import random
 
+#initilize game
 pygame.init()
 
+#set constants
 WIDTH = 800
 HEIGHT = 600
 
-running = True
-paused = True
-
-clock = pygame.time.Clock()
-font = pygame.font.SysFont("calibri", 40)
-
+#set screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption("Pong")
-
 back = pygame.Surface((WIDTH, HEIGHT))
 background = back.convert()
 background.fill((0, 0, 0))
 
+#create Player class
 class Player():
     x = 0
     y = 0
     speed = 250.0
     move = 0
     score = 0
-    height = 76
+    height = 75
     width = 10
     image = pygame.Surface((width, height)).convert()
     image.fill((0, 0, 255))
 
-class Ball():
-    size = 16
-    x = WIDTH/2-size/2
-    y = HEIGHT/2-size/2
-    xMove = 250
-    yMove = 250
-    speed = 1
-    image = pygame.Surface((size,size))
-    pygame.draw.circle(image,(0,255,0),(int(size/2),int(size/2)),int(size/2))
-    image = image.convert()
-
+#create players
 player1 = Player()
 player2 = Player()
-ball = Ball()
 
-player1.x = 10
-player1.y = HEIGHT/2 - player1.height/2
+#set player variables to values
+player1.x, player1.y = 10, HEIGHT/2 - player1.height/2
+player2.x, player2.y = WIDTH - 10 - player2.width, HEIGHT/2 - player2.height/2
 
-player2.x = WIDTH - 10 - player2.width
-player2.y = HEIGHT/2 - player2.height/2
+#set clock
+isRunning = True
+clock = pygame.time.Clock()
 
-while running:
-
+#game loop
+while isRunning:
+    #event loop
     for event in pygame.event.get():
+        #quit game
         if event.type == QUIT:
-            running = False
+            isRunning = False
+        #keyboard controller
         elif event.type == KEYDOWN:
             if event.key == K_w:
                 player1.move = -player1.speed
-            elif event.key == K_s:
+            if event.key == K_s:
                 player1.move = player1.speed
-            elif event.key == K_UP:
+            if event.key == K_UP:
                 player2.move = -player2.speed
-            elif event.key == K_DOWN:
+            if event.key == K_DOWN:
                 player2.move = player2.speed
-            elif event.key == K_SPACE:
-                paused = False
-                ball.xMove = random.randint(100, 250)
-                if random.randint(0, 1) == 0:
-                    ball.xMove = -ball.xMove
-                ball.yMove = random.randint(100, 250)
-                if random.randint(0, 1) == 0:
-                    ball.yMove = -ball.yMove
-        elif event.type == KEYUP:
-            if event.key == K_w or event.key == K_s:
-                player1.move = 0
-            elif event.key == K_UP or event.key == K_DOWN:
-                player2.move = 0
-
+                
+    #standardize clock and movement speed
     timePassed = clock.tick(30)
-    timeSec = timePassed/1000.0
+    timeSecond = timePassed/1000.0
+    player1.y += player1.move*timeSecond
+    player2.y += player2.move*timeSecond
 
-    if not paused:
-        player1.y += player1.move*timeSec
-        player2.y += player2.move*timeSec
-        ball.x += ball.xMove*timeSec*ball.speed
-        ball.y += ball.yMove*timeSec*ball.speed
-        ball.speed += .002
-
+    #set boundraries 
     if player1.y >= HEIGHT - player1.height - 10:
         player1.y = HEIGHT - player1.height - 10
+        player1.move = -player1.move
     elif player1.y <= 10 :
         player1.y = 10
+        player1.move = -player1.move
     if player2.y >= HEIGHT - player2.height - 10:
         player2.y = HEIGHT - player2.height - 10
+        player2.move = -player2.move
     elif player2.y <= 10:
         player2.y = 10
+        player2.move = -player2.move
 
-    if ball.y <= 10.0:
-        ball.yMove = -ball.yMove
-        ball.y = 10.0
-    elif ball.y >= HEIGHT - 10 - ball.size:
-        ball.yMove = -ball.yMove
-        ball.y = HEIGHT - 10 - ball.size
-
-    if ball.x <= player1.x + player1.width:
-        if ball.y + ball.size >= player1.y and ball.y <= player1.y + player1.height:
-            ball.x = player1.x + player1.width+5
-            ball.xMove = -ball.xMove
-    if ball.x + ball.size >= player2.x:
-        if ball.y + ball.size >= player2.y and ball.y <= player2.y + player2.height:
-            ball.x = player2.x - player2.width-5
-            ball.xMove = -ball.xMove
-
-    if ball.x < 5.0:
-        player2.score += 1
-    elif ball.x > WIDTH-5-ball.size:
-        player1.score += 1
-
-    if ball.x < 5.0 or ball.x > WIDTH-5-ball.size:
-        ball.x = WIDTH/2 - ball.size/2
-        ball.y = HEIGHT/2 - ball.size/2
-        player1.y = HEIGHT/2 - player1.height/2
-        player2.y = HEIGHT/2 - player2.height/2
-        paused = True
-        ball.speed = 1
-
+    #add images to gamewindow
     screen.blit(background, (0, 0))
     pygame.draw.rect(screen,(255,255,255),Rect((5,5),(WIDTH - 10, HEIGHT - 10)),2)
     pygame.draw.aaline(screen, (255,255,255),(int(WIDTH/2),5),((int(WIDTH/2),HEIGHT-5)))
     screen.blit(player1.image, (player1.x, player1.y))
-    screen.blit(player1.image, (player2.x, player2.y))
-    screen.blit(ball.image, (ball.x,ball.y))
-
-    score1 = font.render(str(player1.score), True,(255,255,255))
-    score2 = font.render(str(player2.score), True,(255,255,255))
-    screen.blit(score1, (15, 15))
-    screen.blit(score2, (WIDTH/2+15, 15))
-
-    if(paused):
-        screen.blit(font.render("Press SPACE to Start Round",True,(255,255,255)), (WIDTH/2 - 210, HEIGHT/2 - 70))
-
-
+    screen.blit(player2.image, (player2.x, player2.y))
+    
     pygame.display.update()
 
+#end game loop and quit
 pygame.quit()
