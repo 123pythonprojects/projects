4ddda6e261624e1f6786eaca6bb3797613a1a220
Chris Lardner <sanantonio2968@gmail.com>
2023-06-29 06:32:06 -0700

    Update createAClass.py

---
+++ createAClass.py
@@ -0,0 +1,32 @@
+'''
+Create your own class
+
+'''
+
+'''
+INSTRUCTIONS:
+
+Mark "TODO" as DONE as you complete the tasks
+
+TODO: Create a class
+        example: Book
+        example: Mobile phone
+        example: Pigeon
+TODO: Create parameters for the class
+        example: A book has a title, a number of pages, and an author
+        example: A mobile phone has a brand, GBs of storage, and a model
+        example: A pigeon has a name, city, and an age
+TODO: Create at least 2 functions for your class
+        example: Certain numbers of a book can be read, the book can be completed
+        example: A mobile phone can place calls, and you can text people
+        example: A pigeon can deliver messages, or eat bread crumbs
+TODO: Create at least 2 instances of your object
+        example: harry = Book("Harry Potter and the blah blah", 450, "JK Rowling")
+        example: my_phone = Phone("Apple", 256, "iPhone 100")
+        example: steve = Pigeon("Steve", "New York City", 2)
+TODO: Call on the functions of your class
+        example: harry.read(200)
+        example: my_phone.text("Steve")
+        example: steve.deliver(letter, "Alex")
+
+'''
