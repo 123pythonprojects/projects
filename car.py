8f038338b6ebcb258b67d5ea9602a7eec4188f63
Chris Lardner <sanantonio2968@gmail.com>
2023-06-29 05:26:29 -0700

    update cat.py

--- car.py
+++ car.py
@@ -1,9 +1,36 @@
 '''
 A car class!
 '''
-class Car:
-    def __init__(self):
-        pass
-    def drive(self):
-        pass
+class Cat:
+    def __init__(self, name):
+        self.name = name
+        self.will_purr = False
+    def meow(self):
+        print("meow")
+
+david = Cat("David")
+
+print("")
+
+print("Hello and congratulations, you have won a free cat! What is your name? \n")
+
+name = str(input())
+print("")
+print("Hello " + name + ". Do you like cats? (y/n)\n")
+
+likes_cats = input()
+print("")
+
+if likes_cats == 'y':
+    david.will_purr = True
+
+print("Excellent choice. Would you like to pet the cat? (y/n)\n")
+
+choice = input()
+print("")
+
+if david.will_purr:
+    print(david.name + " the cat purred. David is very happy, " + name + ". \n")
+else:
+    print(david.name + " the cat said " + david.meow + ". He does not like you, " + name + ".\n")
 
